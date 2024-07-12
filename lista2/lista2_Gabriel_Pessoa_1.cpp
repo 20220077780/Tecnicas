888374c80976ea58add68a290abebc4fc2b5bc66
20220077780 <gabriel.pessoa@estudante.cear.ufpb.br>
2024-07-12 17:08:41 -0300

    Update lista2_Gabriel_Pessoa_1.cpp

--- lista2/lista2_Gabriel_Pessoa_1.cpp
+++ lista2/lista2_Gabriel_Pessoa_1.cpp
@@ -1,10 +1,3 @@
-/*                  Seja bem vindo ao arquivo de teste! A função desse arquivo é
-                        ser um espaço para você testar qualquer programa que queira
-            
-                    Esse arquivo não é referente a um exercício específico!
-
-                    O executavel desse arquivo tem o nome "adt1"! Compile no terminal com "./adt1"  */
-
 //Imprima na tela seu nome completo e o valores entre 0 e sua idade em linhas separadas.
 #include <iostream>
 #include <string>
@@ -20,9 +13,3 @@ int main(){
         }
 return 0;
 }
-
-
-
-
-
-

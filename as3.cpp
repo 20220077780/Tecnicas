d6b9d79915f5ca2e5de07a5da9cad55b76ab1441
20220077780 <gabriel.pessoa@estudante.cear.ufpb.br>
2024-07-12 13:31:39 -0300

    Update as3.cpp

--- as3.cpp
+++ as3.cpp
@@ -1,1 +1,17 @@
+//Imprima o diâmetro, o perímetro e a área de um círculo cujo raio seja dado pelo usuário. Utilize o operador #define para definir o valor de pi.
 #include <iostream>
+#include <cmath>
+#define pi acos(-1)
+using namespace std;
+int DPA[3](int a)
+
+int main(){
+  int r;
+  cout << "Hey brother! Type here bellow the radius of a circule: \n";
+  cin >> r;
+  cout << "Pi = " << pi;
+  
+  
+
+return 0;
+}

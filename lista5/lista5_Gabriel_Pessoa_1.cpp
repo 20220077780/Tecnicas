a9249176bdddf90a94cf71825ee3e27cf997dd29
Gabriel Pessoa <gabriel.pessoa@estudante.cear.ufpb.br>
2024-08-09 19:16:48 -0300

    cin.ignore()

--- lista5/lista5_Gabriel_Pessoa_1.cpp
+++ lista5/lista5_Gabriel_Pessoa_1.cpp
@@ -19,19 +19,12 @@ V. Tenha uma opção de retornar à tela inicial.*/
 using namespace std;
 
 int cadastroI();
-//void cadastroII(int);
-//void cadastroIII(int);
-//void cadastroIV(int);
-//void cadastroV(int);
-//void cadastroVI(int);
-//void cadastroVII(int);
-//void cadastroVIII(int);
-//void cadastroIX(int);
-//void cadastroX(int);
+int listaII(int);
+int obesa(int);
 
 class pessoa{
-	int idade, CPF;
-	float renda, altura, peso;
+	int idade;
+	float renda, altura, peso, CPF;
 	string nome, estado;
 	public:
 
@@ -40,7 +33,7 @@ class pessoa{
 		int obesidade = (IMC(peso, altura)>=30)? 1:0; //if(obesidade) cout << "OBESO!\n";
 
 		void SETidade(int a){idade = a;}		; int GETidade(){return idade;}
-		void SETCPF(int a){CPF = a;}			; int GETCPF(){return CPF;}
+		void SETCPF(float a){CPF = a;}			; float GETCPF(){return CPF;}
 		void SETrenda(float a){renda = a;}		; float GETrenda(){return renda;}
 		void SETaltura(float a){altura = a;}	; float GETaltura(){return altura;}
 		void SETpeso(float a){peso = a;}		; float GETpeso(){return peso;} 
@@ -50,7 +43,7 @@ class pessoa{
 
 pessoa people[10]; //cadastro como variavel global
 int main(){
-	int c; 
+	int c, k=0;; 
 	cout << "Ola, qual opção tu queres fazer?\n"
 		<< "\t[1] Cadastrar nova pessoa\n"
 		<< "\t[2] Lista de um dado especifico\n"
@@ -60,20 +53,24 @@ int main(){
 	cin >> c;
 	switch(c){
 		case 0: break;
-		case 1: c=cadastroI(); break;
-		case 2: break;
+		case 1: k =cadastroI(); break;
+		case 2: c=listaII(k); break;
 		case 3: break;
-		case 4: break;
+		case 4: c=obesa(k); break;
 		default: cout << "Por favor, digite uma opcao valida!\n"; break;
 	}
+/*	for(int i = 0; i<10; i++){
+	    cout << "Nome " << i+1 << "= " << people[i].GETnome() << endl;
+	}
+*/	
 return 0;
 }
 
 int cadastroI(){
 
-	int idade, CPF;
-	float renda, altura, peso;
-	string nome, estado;
+	int idade;
+	float renda, altura, peso, CPF;
+	string nome="none"; string estado="None";
 
 	cout << "Ola, vamos cadastrar uma pessoa?\nQuantas pessoas serao cadastradas dessa vez?\n";
 	int c;
@@ -81,6 +78,7 @@ int cadastroI(){
 	if(c>10){cout << "Perdao, somente cadastramos 10 pessoas por vez, reinicie o programa apos o cadastramento das 10 primeiras.\n"; c=10;}
 	for(int i=0; i<c; i++){
 		cout << "Nome " << i+1 << ": ";
+		    cin.ignore();
 			getline(cin, nome);
 			people[i].SETnome(nome);
 		cout << "Idade de " << nome << ": ";
@@ -99,13 +97,57 @@ int cadastroI(){
 			cin >> peso;
 			people[i].SETpeso(peso);
 		cout << "Estado civil de " << nome << ": ";
+		    cin.ignore();
 			getline(cin, estado);
 			people[i].SETestado(estado);
 	}
 
-	cout <<"Deseja retornar para a tela inicial?\n\t[0] NAO\n\t[Qualquer numero] SIM\n";
-	cin >> idade;
-	return idade;
+    cout << "Voce foi redirecionado para a tela inicial!\n";
+	return c;
 }
 
-//seria que foi?
+int listaII(int quantidade){
+    int c, i;
+    cout << "Qual das informacoes tu queres saber?:\n"
+		<< "\t[1] Nome\n"
+		<< "\t[2] Idade\n"
+		<< "\t[3] CPF\n"
+		<< "\t[4] Renda\n"
+		<< "\t[5] Altura\n"
+		<< "\t[6] Peso\n"
+		<< "\t[7] Estado civil\n";
+	cin >> c;
+	switch(c){
+	    case 1: for( i=0; i<quantidade; i++){cout << "Pessoa " << i+1 << " = " << people[i].GETnome() << endl;}; break;
+	    case 2: for( i=0; i<quantidade; i++){cout << "Idade de " << people[i].GETnome() << " = " << people[i].GETidade() << endl;}; break;
+	    case 3: for( i=0; i<quantidade; i++){cout << "CPF de " << people[i].GETnome() << " = " << people[i].GETCPF() << endl;}; break;
+	    case 4: for( i=0; i<quantidade; i++){cout << "Renda de " << people[i].GETnome() << " = " << people[i].GETrenda() << endl;}; break;
+	    case 5: for( i=0; i<quantidade; i++){cout << "Altura de " << people[i].GETnome() << " = " << people[i].GETaltura() << endl;}; break;
+	    case 6: for( i=0; i<quantidade; i++){cout << "Peso de " << people[i].GETnome() << " = " << people[i].GETpeso() << endl;}; break;
+	    case 7: for( i=0; i<quantidade; i++){cout << "Estado civil de " << people[i].GETnome() << " = " << people[i].GETestado() << endl;}; break;
+	    default: cout << "Valor inválido.";
+	    
+	}
+
+	cout <<"Deseja retornar para a tela inicial?\n\t[0] NAO\n\t[Qualquer numero] SIM\n";
+    cin >> c;
+	return c;   
+}
+//IV. Imprima o IMC (índice de massa corporal) de todos os usuários cadastrados juntamente com a informação se a pessoa está obesa ou não.
+//float IMC(float peso, float altura) {return peso/(altura*altura);}
+//int obesidade = (IMC(peso, altura)>=30)? 1:0; //if(obesidade) cout << "OBESO!\n";
+int obesa(int quantidade){
+    cout << "Segue o IMC de cada um dos usuarios cadastrados: \n";
+    for(int i=0; i<quantidade; i++){
+        float peso = people[i].GETpeso();
+        float altura = people[i].GETaltura();
+        cout << "O ICM de " << people[i].GETnome() << " é " << people[i].IMC( peso, altura) << endl;
+        int obesidade = (people[i].IMC(peso, altura)>=30)? 1:0; if(obesidade) cout << "OBESO!\n";
+    }
+    
+    
+    int c;
+	cout <<"Deseja retornar para a tela inicial?\n\t[0] NAO\n\t[Qualquer numero] SIM\n";
+    cin >> c;
+	return c;
+}
---
+++ lista5/teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string nomezin;
+	cout << "Digite seu nomezin baby: ";
+	getline(cin, nomezin);
+	cout << "Nomezin = " << nomezin << endl;
+
+return 0;
+}

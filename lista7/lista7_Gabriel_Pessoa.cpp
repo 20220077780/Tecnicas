00b043d147c00163aa13680bf6bc0f0ab7956414
Gabriel Pessoa <gabriel.pessoa@estudante.cear.ufpb.br>
2024-09-09 19:13:08 -0300

    up

---
+++ lista7/lista7_Gabriel_Pessoa.cpp
@@ -0,0 +1,20 @@
+/*
+Crie uma classe para representar uma MATRIZ QUADRADA de ORDEM 
+3 que contenha como variável privada uma array bidimensional para 
+armazenar os elementos da matriz. Crie funções e/ou variáveis-membro 
+que desejar.
+a) Elabore um programa que tenha opção para o usuário escolher uma 
+das tarefas abaixo:
+I. Crie um objeto da classe e usando um ponteiro para este objeto 
+inicialize a array do objeto com valores dados pelo usuário e por meio 
+de uma função-membro. Após, por meio de uma outra função membro, imprima o determinante da matriz. 
+II. Crie uma array de 10 objetos da classe e, acessando-os via um só 
+ponteiro, inicialize cada objeto com valores pseudoaleatórios 
+inteiros entre -5 e 5 usando uma função-membro. Após, imprima 
+todos os determinantes dos objetos.
+III. Crie 2 objetos da classe e os inicialize com valores dados pelo 
+usuários. Após, usando uma função que tenha como parâmetros de 
+entrada, 2 ponteiros para os objetos criados, imprima a soma dos 
+determinantes dos objetos. 
+IV. Tenha uma opção de retornar à tela inicial.
+*/

f15b4f316ed198bfa606954649cb1a4a93664975
20220077780 <gabriel.pessoa@estudante.cear.ufpb.br>
2024-09-20 13:32:27 -0300

    aCHO QUE NAO VAI DAR PRA VOIDIAR ;-;

--- lista9/lista9_Gabriel_Pessoa.cpp
+++ lista9/lista9_Gabriel_Pessoa.cpp
@@ -20,40 +20,115 @@ em todas as disciplinas matriculadas.
 #include <fstream>
 #include <iostream>
 #include <cstring>
+#include <iomanip>
 using namespace std;
+
+void receber();
+void listar();
+void search();
+void notas();
+void aluno();
 
 int main(int argc, char *argv[]){
     //string disciplina, nome;
-    float Ni, Nii, Niii;
+    int x=0;
     fstream SIGAA("sigaa.txt", ios::app);
     if(!SIGAA){
         cout << "The file can't be open!\n";
         return 1;
     }
     if(argc == 6){
+        float Ni, Nii, Niii;
         char disciplina[30], nome[30];
-        strcpy(disciplina, argv[1]);
-        strcpy(nome, argv[2]);
-        Ni = atof(argv[3]);
+        strcpy(disciplina, argv[1]);    
+        strcpy(nome, argv[2]);          
+        Ni = atof(argv[3]);             
         Nii = atof(argv[4]);
         Niii = atof(argv[5]);
 
-        SIGAA << disciplina << "|" << nome << endl
-            << Ni << ' ' << Nii << ' ' << Niii << endl;
+        SIGAA << left << setw(30) << disciplina << setw(30) << nome << setw(5) << Ni << setw(5) << Nii << setw(5) << Niii << endl;
+        
     }
     else{
-        string disciplina, nome;
-        cout << "Which subject?" << endl;                       cin.ignore(); SIGAA.getline(disciplina, 30);    //getline(cin, disciplina); // Nao pega a primeira letra!
-        cout << "What is the name of the student?" << endl;     cin.ignore(); SIGAA.getline(nome, 30);          //getline(cin, nome);
+        int c;
+        while(1){
+            if(!(x++)) cout << "Hello, welcome! What would you like to do?\n";
+            else cout << "And now?\n"; // deu certooo
+            cout << "[0] Leave" << endl
+                 << "[1] New register " << endl
+                 << "[2] Subject list " << endl
+                 << "[3] Research " << endl
+                 << "[4] Note list " << endl
+                 << "[5] Student profile " << endl;
+            cin >> c;
+            switch(c){
+                case 0: break;
+                case 1: {
+                        float Ni, Nii, Niii;
+                        string disciplina, nome;
+                        cout << "Which subject?" << endl;                       cin.ignore(); getline(cin, disciplina); disciplina.resize(39, ' ');
+                        cout << "What is the name of the student?" << endl;     getline(cin, nome);                     nome.resize(39, ' ');
+                        cout << "First note: ";                                 cin >> Ni;
+                        cout << "Second note: ";                                cin >> Nii;
+                        cout << "Thrird note: ";                                cin >> Niii;
+                
+                        SIGAA << left << setw(40) << disciplina << '|' << setw(39) << nome << '|' << setw(4) << Ni << '|' << setw(4) << Nii << '|' << setw(4) << Niii<< endl; // tamanho da linha = 40+39+4+4+4+(4)+(2)= 97 
+                        continue;
+                }
+                case 2:{
+                        int i=0;
+                        char c;
+                        SIGAA.seekg(0);
+                        while(c!='|'){
+                            SIGAA >> c;
+                            cout << c;
+                        }
+                        continue;
+                }
+                case 3: search(); continue;
+                case 4: notas(); continue;
+                case 5: aluno(); continue;
+                default: cout << "Put an valid number!\n"; continue;
+            }
+            break;
+        }
+        cout << "Thanks for your time!" << endl;
+    }
+    
+    SIGAA.close();
+    return 0;
+}
+
+void receber(){
+//a) Receber novos dados, salvando na base de dados;
+    float Ni, Nii, Niii;
+    string disciplina, nome;
+        cout << "Which subject?" << endl;                       getline(cin, disciplina); // Nao pega a primeira letra!
+        cout << "What is the name of the student?" << endl;     getline(cin, nome);
         cout << "First note: ";                                 cin >> Ni;
         cout << "Second note: ";                                cin >> Nii;
         cout << "Thrird note: ";                                cin >> Niii;
 
-        SIGAA << disciplina << "|" << nome << endl
-            << Ni << ' ' << Nii << ' ' << Niii << endl;
-    }
-    
+        //SIGAA << disciplina << "|" << nome << endl
+        //    << Ni << ' ' << Nii << ' ' << Niii << endl;
+}
 
-    SIGAA.close();
-    return 0;
+void listar(){
+//b) Listar todas as disciplinas e os alunos matriculados em cada;
+    
+}
+
+void search(){
+//c) Ter opção de pesquisar se um dado aluno ou uma dada disciplinas está cadastrada.
+    
+}
+
+void notas(){
+//d) Listar as notas, parciais ou não, e a média dos alunos de uma dada disciplina cadastrada e a média atual, indicando se reprovado (média <7) ou aprovado (média >=7).  
+
+}
+
+void aluno(){
+//e) Listar as notas, parciais ou não, e média de um dado aluno em todas as disciplinas matriculadas.    
+
 }
